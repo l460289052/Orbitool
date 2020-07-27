f30d2de93148908ed5a4fe53f6b5633fd09bf7c5
l4602 <l460289052@163.com>
2020-07-27 17:56:26 +0800

    .gitignore

---
+++ tests/test_file.py
@@ -0,0 +1,61 @@
+import multiprocessing
+import os
+from datetime import datetime, timedelta
+
+import pytest
+
+from utils import files
+
+
+def test_FileTraveler_noRecurrent():
+    ft = files.FolderTraveler(".", '.py', False)
+    thisFile=os.path.split(__file__)[1]
+    find = False
+    for f in ft:
+        if os.path.split(f)[1] == thisFile:
+            find = True
+    assert not find
+
+
+def test_FileTraveler_recurrent():
+    ft = files.FolderTraveler(".", '.py', True)
+    thisFile=os.path.split(__file__)[1]
+    find = False
+    for f in ft:
+        if os.path.split(f)[1] == thisFile:
+            find = True
+    assert find
+
+class TenMinuteFile(files.File):
+    def __init__(self, creationDatetime: datetime):
+        self.path = datetime.now()
+        self.creationDatetime = creationDatetime
+        self.startTimedelta = timedelta()
+        self.endTimedelta = timedelta(minutes=10)
+
+
+class TestFileList:
+    def setup_class(self):
+        self.lf = files.FileList()
+        self.ref = datetime.now()
+        for i in range(10):
+            self.lf.addFile(TenMinuteFile(self.ref + timedelta(minutes=15) * i))
+
+    def test_add(self):
+        with pytest.raises(ValueError):
+            self.lf.addFile(TenMinuteFile(self.ref + timedelta(minutes=5)))
+    
+    def test_timeRange(self):
+        assert self.lf.timeRange() == (self.ref, self.ref+timedelta(minutes=15*9+10))
+
+
+
+
+
+
+
+
+    
+    
+    
+    
---
+++ tests/test_thermo.py
@@ -0,0 +1,10 @@
+from utils.readers import ThermoFile
+from PyQt5 import QtWidgets
+
+class TestThermo:
+    def setup_class(self):
+        f, _ = QtWidgets.QFileDialog.getOpenFileName(directory="./..")
+        self.f = ThermoFile(f)
+
+    def test_average(self):
+        self.f.getAveragedSpectrum(1e-6,None,(1,10))
