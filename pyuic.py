cf636163e3f3a3833d92a113a37a9b902449ffa8
l4602 <l460289052@163.com>
2021-01-17 15:37:25 +0800

    ui_init

--- pyuic.py
+++ pyuic.py
@@ -1,14 +1,15 @@
 import os
 
+
 def main():
-    for ui in os.listdir('.'):
+    dirpath = "Orbitool/UI"
+    for ui in os.listdir(dirpath):
         if os.path.splitext(ui)[1].lower() == '.ui':
-            uipy=os.path.splitext(ui)[0]+'Ui.Py'
-            if os.path.exists(uipy):
-                if os.path.getmtime(ui) > os.path.getmtime(uipy):
-                    os.system('pyuic5 %s -o %s'%(ui,uipy))
-            else:
-                os.system('pyuic5 %s -o %s'%(ui,uipy))
+            ui = os.path.join(dirpath, ui)
+            uipy = os.path.splitext(ui)[0]+'Ui.Py'
+            if not os.path.exists(uipy) or os.path.getmtime(ui) > os.path.getmtime(uipy):
+                os.system(f'pyuic5 {ui} -o {uipy}')
+
 
 if __name__ == "__main__":
     main()
