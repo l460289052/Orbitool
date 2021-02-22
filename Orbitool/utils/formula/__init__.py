21610ee121985d6fa8520b364934f91a71879c71
l4602 <l460289052@163.com>
2021-02-22 16:19:38 +0800

    mv formula

--- Orbitool/utils/formula/__init__.py
+++ Orbitool/utils/formula/__init__.py
@@ -3,5 +3,3 @@ from . import _restrictedCalc
 from ._formula import Formula
 from ._restrictedCalc import Calculator as RestrictedCalc
 from ._forceCalc import Calculator as ForceCalc
-
-from .hdf5 import FormulaDescriptor as HDF5FormulaDescriptor, FormulaDatatableDescriptor as HDF5FormulaDatatableDescriptor
