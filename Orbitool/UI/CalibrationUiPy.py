447f6986db837663a078e47733269a9704febf28
l4602 <l460289052@163.com>
2021-07-01 11:10:29 +0800

    add & rm ions

--- Orbitool/UI/CalibrationUiPy.py
+++ Orbitool/UI/CalibrationUiPy.py
@@ -1,9 +1,10 @@
 from typing import Union, Optional
 from . import CalibrationUi
-from .manager import Manager
+from .manager import Manager, state_node
 from PyQt5 import QtWidgets, QtCore
 from ..utils.formula import Formula
 from ..structures.workspace.calibration import Ion
+from .utils import get_tablewidget_selected_row
 
 
 class Widget(QtWidgets.QWidget, CalibrationUi.Ui_Form):
@@ -17,16 +18,16 @@ class Widget(QtWidgets.QWidget, CalibrationUi.Ui_Form):
     def setupUi(self, Form):
         super().setupUi(Form)
 
+        self.addIonToolButton.clicked.connect(self.addIon)
+        self.delIonToolButton.clicked.connect(self.removeIon)
+
     @property
     def calibration(self):
         return self.manager.workspace.calibration_tab
 
     def init(self):
-        info = self.calibration.info
         ions = ['C5H6O9N-', 'C2HO4-', 'C3H4O7N-']
-        for ion in ions:
-            info.ions.append(Ion(shown_text=ion, formula=Formula(ion)))
-
+        self.calibration.info.add_ions(ions)
         self.showIons()
 
     def showIons(self):
@@ -40,3 +41,16 @@ class Widget(QtWidgets.QWidget, CalibrationUi.Ui_Form):
                 index, 1, QtWidgets.QTableWidgetItem(format(ion.formula.mass(), ".4f")))
 
         table.show()
+
+    @state_node
+    def addIon(self):
+        self.calibration.info.add_ions(self.ionLineEdit.text().split(','))
+        self.showIons()
+
+    @state_node
+    def removeIon(self):
+        indexes = get_tablewidget_selected_row(self.tableWidget)
+        ions = self.calibration.info.ions
+        for index in reversed(indexes):
+            ions.pop(index)
+        self.showIons()
--- Orbitool/structures/workspace/calibration.py
+++ Orbitool/structures/workspace/calibration.py
@@ -14,4 +14,12 @@ class Ion(BaseTableItem):
 class CalibratorInfo(BaseStructure):
     h5_type = "calibrator"
 
-    ions: List[Ion]=Field(default_factory=list)
+    ions: List[Ion] = Field(default_factory=list)
+
+    def add_ions(self, ions: List[str]):
+        s = {ion.formula for ion in self.ions}
+        for ion in ions:
+            f = Formula(ion)
+            if f in s:
+                continue
+            self.ions.append(Ion(shown_text=ion, formula=f))
