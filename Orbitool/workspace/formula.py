87063d5206b37b910a473228dfaa669d289658a5
l4602 <l460289052@163.com>
2021-07-06 21:34:51 +0800

    formula calc factory

--- Orbitool/workspace/formula.py
+++ Orbitool/workspace/formula.py
@@ -3,8 +3,19 @@ from ..utils.formula import Formula, ForceCalc, RestrictedCalc
 from ..structures.base import BaseStructure, Field
 
 
+def RestrictedCalcFactory():
+    calc = RestrictedCalc()
+    calc.setEI('N')
+
+
+def ForceCalcFactory():
+    calc = ForceCalc()
+    calc['N'] = 999
+
+
 class FormulaInfo(BaseStructure):
     h5_type = "formula docker"
 
-    restricted_calc: RestrictedCalc = Field(default_factory=RestrictedCalc)
-    force_calc: ForceCalc = Field(default_factory=ForceCalc)
+    restricted_calc: RestrictedCalc = Field(
+        default_factory=RestrictedCalcFactory)
+    force_calc: ForceCalc = Field(default_factory=ForceCalcFactory)
