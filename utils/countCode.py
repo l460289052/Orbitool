deea3c0ca27a520705b01794868eddd5b5cf1b72
l4602 <l460289052@163.com>
2021-07-07 13:08:21 +0800

    count sort

--- utils/countCode.py
+++ utils/countCode.py
@@ -1,18 +1,26 @@
 import os
 import shutil
 from itertools import chain
+from typing import TypedDict
 
 from Orbitool.utils.files import FolderTraveler
 
-exts = ['.py', '.pyx', '.pxd']
+exts = ['.py', '.pyx', '.pxd', '.pyi']
 
 
-def count(recurrent_path, not_recurrent_path, count_blank_line = False):
+class File(TypedDict):
+    lines: int
+    size: float
+    name: str
+
+
+def count(recurrent_path, not_recurrent_path, count_blank_line=False):
     cnt = 0
     size = .0
     fts = [FolderTraveler(not_recurrent_path, exts, False),
            FolderTraveler(recurrent_path, exts, True)]
 
+    files =[]
     for file in chain.from_iterable(fts):
         if not file.lower().endswith('ui.py'):
             with open(file, 'r', encoding='utf-8') as f:
@@ -21,8 +29,11 @@ def count(recurrent_path, not_recurrent_path, count_blank_line = False):
                     lines = [line for line in lines if len(line) > 5]
 
                 fsize = os.path.getsize(file) / 1024
-                size += fsize
-                print(len(lines), format(fsize, '.4'), 'KB', file)
+                files.append(File(lines=len(lines), size = fsize, name = file))
                 cnt += len(lines)
+                size += fsize
+    files.sort(key=lambda f:f['lines'], reverse=True)
+    for f in files:
+        print(f['lines'], format(f['size'], '.4'), 'KB', f['name'])
 
     print('Total:', cnt, 'lines', format(size, '.4'), 'KB')
